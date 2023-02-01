04dcf02ca92cdc72eca82b030b77cc75dd0c8b16
Wojtek130 <pimpek86@gmail.com>
2023-02-01 22:32:41 +0100

    db setup in progress

---
+++ config/database.js
@@ -0,0 +1,22 @@
+const mode = process.env.MODE || 'development';
+const dialect = process.env.DB_DIALECT || sequelizeConfig[mode].dialect || 'postgres';
+const database = process.env.DB_DATABASE || sequelizeConfig[mode].database || 'shopdb';
+const username = process.env.DB_USERNAME || sequelizeConfig[mode].username || 'root';
+const password = process.env.DB_PASSWORD || sequelizeConfig[mode].password || 'root';
+
+const connectionObject = {
+    host: process.env.DB_HOST || sequelizeConfig[mode].host || 'localhost',
+    dialect, // 'mysql'|'sqlite'|'postgres'|'mssql',
+
+};
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+
+db.User = require('../model/user')(sequelize, Sequelize);
+// here db associations
+
+module.exports = db;
