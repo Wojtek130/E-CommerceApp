a30e95de39162501c999a24fcb9e0bc9c46b2eac
Wojtek130 <pimpek86@gmail.com>
2023-02-01 22:02:11 +0100

    app architecture ready

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require('cors');
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.resolve(__dirname, "../greengrocers/assets")));
+
+module.exports = app;
