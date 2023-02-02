21f2b5ec94313d58dc7046fdccca2113f4dd39f2
Wojtek130 <pimpek86@gmail.com>
2023-02-02 17:50:34 +0100

    connection object fixed

--- config/database.js
+++ config/database.js
@@ -1,20 +1,23 @@
+const Sequelize = require('sequelize');
+
 const mode = process.env.MODE || 'development';
-const dialect = process.env.DB_DIALECT || sequelizeConfig[mode].dialect || 'postgres';
-const database = process.env.DB_DATABASE || sequelizeConfig[mode].database || 'shopdb';
-const username = process.env.DB_USERNAME || sequelizeConfig[mode].username || 'root';
-const password = process.env.DB_PASSWORD || sequelizeConfig[mode].password || 'root';
+const dialect = process.env.DB_DIALECT || 'postgres';
+const database = process.env.DB_DATABASE || 'shopdb';
+const username = process.env.DB_USERNAME || 'root';
+const password = process.env.DB_PASSWORD || 'root';
 
 const connectionObject = {
-    host: process.env.DB_HOST || sequelizeConfig[mode].host || 'localhost',
+    host: process.env.DB_HOST ||  'localhost',
     dialect, // 'mysql'|'sqlite'|'postgres'|'mssql',
 
 };
+
+const sequelize = new Sequelize(database, username, password, connectionObject);
 
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-
 
 db.User = require('../models/user.model')(sequelize, Sequelize);
 // here db associations
