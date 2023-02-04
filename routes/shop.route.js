29c24da0ff3b036ee4e4db6329ec0658a22257de
Wojtek130 <pimpek86@gmail.com>
2023-02-04 21:25:07 +0100

    autentication ready

--- routes/shop.route.js
+++ routes/shop.route.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const shopController = require("../controllers/shop.controller");
+const auth = require("../middlewares/auth.middleware");
 
 router.get("/products", shopController.AllProductsWithTags);
 router.get("/api", shopController.helloFun);
 router.post("/login", shopController.login);
 router.post("/register", shopController.register);
-
+router.post("/welcome", auth, (req, res) => {
+    res.status(200).send("Welcome 🙌 ");
+  });
 
 module.exports = router;
