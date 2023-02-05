60bb17053a5d23397c23ebe918d2dbb790262e6c
Wojtek130 <pimpek86@gmail.com>
2023-02-05 13:28:38 +0100

    /completeOrder tested and working

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -1,6 +1,7 @@
 const {
   getProductWithTags,
   createSingleOrder,
+  createFullOrder,
 } = require("../services/shop.service");
 
 const helloFun = function (req, res) {
@@ -15,14 +16,15 @@ const AllProductsWithTags = async function (req, res) {
 const completeOrder = async function (req, res) {
   // orderedProducts = [{productId : 5, quantity : 3}, {productId : 6, quantity : 5}]
   const { userId, orderedProducts } = req.body;
-  const fullOrderInfo = { userId: userId };
+  const fullOrderInfo = { UserId: userId };
   const fullOrder = await createFullOrder(fullOrderInfo);
   const fullOrderId = fullOrder.dataValues.id;
+  console.log(fullOrder.dataValues);
   for (const p of orderedProducts) {
     const singleOrderInfo = {
       productId: p.productId,
       quantity: p.quantity,
-      fullOrderId: fullOrderId,
+      FullOrderId: fullOrderId,
     };
     await createSingleOrder(singleOrderInfo);
   }
