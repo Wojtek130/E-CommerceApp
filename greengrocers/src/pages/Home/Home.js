f73a6d981bc623e3bad8f71828d3f1981ca4cc31
Wojtek130 <pimpek86@gmail.com>
2023-02-06 00:26:22 +0100

    btn added

--- greengrocers/src/pages/Home/Home.js
+++ greengrocers/src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Filters from "../../components/Filters.component";
 import MainHeader from "../../components/MainHeader.component";
 import Gallery from "../../components/Gallery.component";
+import "./Home.scss";
 
 const Home = () => {
   return (
